146085dbe278a3cd2d238b11978171502e092a35
jay <jaycast2012@DESKTOP-BGBL0U0.localdomain>
2023-06-18 10:19:28 -0400

    fruitsearch.js

--- fruitsearch.js
+++ fruitsearch.js
@@ -18,27 +18,72 @@ document.addEventListener('keyup', (event)=>{
 
 function search(str) {
 	let results = [];
-
 	// TODO
-
+    let val = str.toLowerCase();
+    console.log(val);
+    
+      for (let i of fruit){
+        if(i.toLowerCase().indexOf(val) > -1) {
+            results.push(i);
+        }
+      }
 	return results;
 }
 
 function searchHandler(e) {
 	// TODO
+    const inputVal = e.target.value;
+	console.log(inputVal);
+	let results = [];
+	if(inputVal.length > 0){
+		results = search(inputVal);
+	}
+    showSuggestions(results, inputVal);
 }
 
 
 function showSuggestions(results, inputVal) {
 
 	// TODO
-}
+    suggestions.innnerHTML = '';
+
+    if(results.langth > 0){
+        for(i=0; i<results.length; i++){
+            let item = results [i];
+            console.log(RegExp(inputVal, "i"))
+            const match = item.match(new RegExp(inputVal,"i"));
+
+            console.log(match);
+
+             item = item.replace(match[0], `<strong>${match[0]}</strong>`);
+
+            suggestions.innerHTML += `<li>${item}</li>`;
+
+        }
+
+        suggestions.classList.add('has-suggestions');
+
+    } else {
+
+        results = [];
+
+        suggestions.innerHTML = '';
+
+        suggestions.classList.remove('has-suggestions');
+
+    }
+        }
+    
+
 
 
 function useSuggestion(e) {
 	// TODO
+    input.value = e.target.innerText;
+    suggestions.innerHTML = '';
+    suggestions.classList.remove('has-suggestions');
 }
 
 
-//input.addEventListener('keyup', searchHandler);
-//suggestions.addEventListener('click', useSuggestion);
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
